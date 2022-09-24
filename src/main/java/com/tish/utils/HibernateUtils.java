d52dcdd9c1a1637930a7d6112c528c079590050b
Tishchenko666 <s5187565@gmail.com>
2022-09-24 20:00:30 +0200

    Added HibernateUtils to open and close SessionFactory

---
+++ src/main/java/com/tish/utils/HibernateUtils.java
@@ -0,0 +1,29 @@
+package com.tish.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtils {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                    .configure("hibernate.cfg.xml").build();
+
+            Metadata metaData = new MetadataSources(standardRegistry)
+                    .getMetadataBuilder()
+                    .build();
+
+            sessionFactory = metaData.getSessionFactoryBuilder().build();
+        }
+        return sessionFactory;
+    }
+
+    public static void closeSessionFactory() {
+        sessionFactory.close();
+    }
+}
